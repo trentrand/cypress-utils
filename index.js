e376776840c0cc69b37a00f5cc2d819fa5266f53
Trent Rand <contact@trentrand.com>
2020-04-18 23:05:55 -0400

    Create command line interface for stress-test command
    Using Yargs, a command line interface has been created. Invoking this
    script will print out a help and options dialog.
    
    Users can invoke the script with the required `fileIdentifier`
    parameter (e.g. `mySpecName` or `mySpecName.spec.js`) and optional flag
    options to run the not-yet-implemented "stress test" command.

---
+++ index.js
@@ -0,0 +1,32 @@
+const yargs = require('yargs');
+
+var argv = yargs.scriptName('cypress-utils')
+  .usage('$0 <cmd> [args]')
+  .example('$0 foo.spec.js', 'stress test the "foo" spec file')
+  .example('$0 bar', 'stress test the matched `bar.spec.js` file')
+  .command('$0 <fileIdentifier>', 'Stress test a Cypress spec file', (yargs) => {
+    yargs
+      .positional('fileIdentifier', {
+        type: 'string',
+        describe: 'A unique identifier for the spec file to test',
+      })
+      .option('threads', {
+        alias: ['t', 'limit'],
+        type: 'number',
+        description: 'Maximum number of parallel test runners',
+        default: 2,
+      })
+      .option('trialCount', {
+        alias: ['n', 'count'],
+        type: 'number',
+        description: 'Number of trial attempts to run test',
+        default: 4,
+      })
+  })
+  .help()
+  .alias('help', 'h')
+  .demandCommand()
+  .showHelpOnFail(true)
+  .argv
+
+console.log(`Command ran with args: ${argv}`);
